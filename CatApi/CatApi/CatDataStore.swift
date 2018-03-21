9affe561b0f4f48313550ab0271970bd7caba343
Emi Clark <emiko.clark@gmail.com>
2018-03-21 14:52:46 -0400

    download cat images async

--- CatApi/CatApi/CatDataStore.swift
+++ CatApi/CatApi/CatDataStore.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class CatDataStore {
     
@@ -23,4 +24,21 @@ class CatDataStore {
             completion(self.Cats)
         }
     }
+    
+    func downloadImages(url: URL, completion: @escaping(Data)->(UIImage)) {
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { print("data for image is nil"); return }
+            let catImage: UIImage = UIImage(from: data)
+            completion(catImage)
+        }.resume()
+    }
 }
+
+//-----
+
+//            cell.catImage.image = UIImage(data: data)
+//        }
+//        if let data = try? Data(contentsOf: url!)
+//        {
+
--- CatApi/CatApi/MyTableViewController.swift
+++ CatApi/CatApi/MyTableViewController.swift
@@ -20,7 +20,6 @@ class MyTableViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,65 +39,20 @@ class MyTableViewController: UITableViewController {
         return catds.Cats.count
     }
 
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
         
         let url = URL(string: catds.Cats[indexPath.row].image_url!)
-        if let data = try? Data(contentsOf: url!)
-        {
-            cell.catImage.image = UIImage(data: data)
-        }
-
+        catds.downloadImages(url: url, completion: { (catDataImage) in
+            
+            DispatchQueue.main.async {
+                cell.catImage.image = UIImage(data: Data)
+                self.tableView.reloadData()
+            }
+        })
+    
         cell.title.text = catds.Cats[indexPath.row].title
         cell.catDescription.text = catds.Cats[indexPath.row].catDescription
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
